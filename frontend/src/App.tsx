a5e145d3d4f41da52a0443c91591f47bf39ab099
Liudvikas Bajarūnas <bajarunasmain@gmail.com>
2024-06-16 23:54:40 +0300

    Api update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,20 +12,16 @@ function App() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    axios
-      .get(
-        'https://d3b4ef04-f68c-4426-8b10-fbbdabd006ca-dev.e1-eu-north-azure.choreoapis.dev/deploytest/backend/v1.0/type/'
-      )
-      .then((response) => {
-        setLoading(false);
-        const { status, data } = response;
+    axios.get('https://api.bajarunas.online/api/type/').then((response) => {
+      setLoading(false);
+      const { status, data } = response;
 
-        if (status === 200) {
-          setData(data);
-        } else {
-          setData([]);
-        }
-      });
+      if (status === 200) {
+        setData(data);
+      } else {
+        setData([]);
+      }
+    });
   }, []);
 
   return <div className='App'>{loading ? '...loading' : 'data got'}</div>;
